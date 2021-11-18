d7fbcaa6ed5c78389c5bde999c6848e94008142d
Eric Choo <ericchoo_0520@yahoo.com>
2021-11-18 21:46:45 +0800

    validation in amount textbox input

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const form = document.querySelector("#expenseForm")
 // const description = document.querySelector("#description");
 // const date = document.querySelector("#date");
-// const amount = document.querySelector("#amount");
+const amount = document.querySelector("#amount");
 const table = document.querySelector("#expenseTable");
 
+//Form input validation
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const { description, date, amount } = form.elements;
@@ -14,19 +15,30 @@ form.addEventListener("submit", (e) => {
     }
 })
 
+//Add new list in the expense table
 function addList(a, b, c) { 
     let tr = document.createElement("tr");
     let tdDescribe = document.createElement("td");
     let tdDate = document.createElement("td");
     let tdAmount = document.createElement("td");
+    const formatNum = parseFloat(c).toFixed(2);
     table.append(tr);
-    tr.append(tdDescribe);
-    tr.append(tdDate);
-    tr.append(tdAmount);
+    tr.append(tdDescribe, tdDate, tdAmount);
     tdDescribe.append(a);
     tdDate.append(b);
-    tdAmount.append(c);
+    tdAmount.append(`$ ${formatNum}`);
+}
+
+// Only allows Numbers and one decimal pont in textbox input
+function isNumberKey(evt){
+    let charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode == 46 && this.value.indexOf('.') != -1){
+        return false;
+    } else if (charCode > 31 && charCode != 46 && (charCode < 48 || charCode > 57 )){
+        return false;
+    } return true;
 }
 
 
- 
+amount.onkeypress = isNumberKey;
+
