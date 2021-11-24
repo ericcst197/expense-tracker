218d8d25567e40a79e15c091a3c0f80efe5cc733
Eric Choo <ericchoo_0520@yahoo.com>
2021-11-24 16:39:05 +0800

    add income & expense balance (with function)

--- script.js
+++ script.js
@@ -1,22 +1,56 @@
-const form = document.querySelector("#expenseForm")
-// const description = document.querySelector("#description");
-// const date = document.querySelector("#date");
-const amount = document.querySelector("#amount");
+const form = document.querySelector("#expenseForm");
 const table = document.querySelector("#expenseTable");
+const select = document.querySelector("#type");
+const total = document.querySelector(".total-balance");
+const income = document.querySelector(".income");
+const expense = document.querySelector(".expense");
+const { description, date, amount } = form.elements;
+
+let zero = 0
+total.innerText = zero.toFixed(2);
+income.innerText = zero.toFixed(2);
+expense.innerText = zero.toFixed(2);
+
+//Selection
+select.addEventListener("change", () => {
+    if (!select.value == "") select.value;
+})
+
 
 //Form input validation
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const { description, date, amount } = form.elements;
     if (!(description.value || date.value || amount.value)) {
         alert("Please fill in the form!")
     } else {
-        addList(description.value, date.value, amount.value)
+        if (select.value === "") {
+            alert("Please select Income/Expense.");
+        } else {
+            select.value === "income" ? valueIncome() : valueExpense();
+            addList(description.value, date.value, amount.value);
+            description.value = "";
+            date.value = "";
+            amount.value = "";
+        }
     };
-    description.value = "";
-    date.value = "";
-    amount.value = "";
 })
+
+//Income value
+function valueIncome() {
+    const calcTotal = parseFloat(total.innerText) + parseFloat(amount.value);
+    const calcIncome = parseFloat(income.innerText) + parseFloat(amount.value);
+    total.innerText = calcTotal.toFixed(2)
+    income.innerText = calcIncome.toFixed(2)
+}
+
+//Expense value
+function valueExpense() {
+    const calcTotal = parseFloat(total.innerText) - parseFloat(amount.value);
+    const calcExpense = parseFloat(expense.innerText) + parseFloat(amount.value);
+    total.innerText = calcTotal.toFixed(2);
+    expense.innerText = calcExpense.toFixed(2);
+}
+
 
 //Add new list in the expense table
 function addList(a, b, c) {
@@ -24,23 +58,43 @@ function addList(a, b, c) {
     const tdDescribe = document.createElement("td");
     const tdDate = document.createElement("td");
     const tdAmount = document.createElement("td");
-    const delBtn = document.createElement("button")
+    const delBtn = document.createElement("button");
     const formatNum = parseFloat(c).toFixed(2);
     table.append(tr);
     tr.append(tdDescribe, tdDate, tdAmount, delBtn);
     tdDescribe.append(a);
     tdDate.append(b);
-    tdAmount.append(`$ ${formatNum}`);
+    tdAmount.append(formatNum);
+    select.value === "income" ? tdAmount.classList.add("income") : tdAmount.classList.add("expense")
     delBtn.innerHTML = "Delete";
     delBtn.addEventListener("click", delList)
 }
 
 //Delete button function
+
 function delList() {
+    const delValueIncome = () => {
+        const calcTotal = parseFloat(total.innerText) - parseFloat(num.innerHTML);
+        const calcIncome = parseFloat(income.innerText) - parseFloat(num.innerHTML);
+        total.innerText = calcTotal.toFixed(2)
+        income.innerText = calcIncome.toFixed(2);
+    };
+    const delValueExpense = () => {
+        const calcTotal = parseFloat(total.innerText) + parseFloat(num.innerHTML);
+        const calcExpense = parseFloat(expense.innerText) - parseFloat(num.innerHTML);
+        total.innerText = calcTotal.toFixed(2);
+        expense.innerText = calcExpense.toFixed(2);
+    }
+
     let li = this.parentNode;
+    let num = this.parentNode.childNodes[2];
+    if (num.classList.value === "income") {
+        delValueIncome()
+    } else {
+        delValueExpense()
+    }
     li.remove()
 }
-
 // Only allows Numbers and one decimal pont in textbox input
 function isNumberKey(evt) {
     let charCode = (evt.which) ? evt.which : evt.keyCode
