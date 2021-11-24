695780893c1b9787e72ad61b0897fd308a001e0c
Eric Choo <ericchoo_0520@yahoo.com>
2021-11-24 11:06:12 +0800

    add delete button

--- script.js
+++ script.js
@@ -8,33 +8,45 @@ const table = document.querySelector("#expenseTable");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const { description, date, amount } = form.elements;
-    if(!(description.value || date.value || amount.value)){
+    if (!(description.value || date.value || amount.value)) {
         alert("Please fill in the form!")
     } else {
         addList(description.value, date.value, amount.value)
-    }
+    };
+    description.value = "";
+    date.value = "";
+    amount.value = "";
 })
 
 //Add new list in the expense table
-function addList(a, b, c) { 
-    let tr = document.createElement("tr");
-    let tdDescribe = document.createElement("td");
-    let tdDate = document.createElement("td");
-    let tdAmount = document.createElement("td");
+function addList(a, b, c) {
+    const tr = document.createElement("tr");
+    const tdDescribe = document.createElement("td");
+    const tdDate = document.createElement("td");
+    const tdAmount = document.createElement("td");
+    const delBtn = document.createElement("button")
     const formatNum = parseFloat(c).toFixed(2);
     table.append(tr);
-    tr.append(tdDescribe, tdDate, tdAmount);
+    tr.append(tdDescribe, tdDate, tdAmount, delBtn);
     tdDescribe.append(a);
     tdDate.append(b);
     tdAmount.append(`$ ${formatNum}`);
+    delBtn.innerHTML = "Delete";
+    delBtn.addEventListener("click", delList)
+}
+
+//Delete button function
+function delList() {
+    let li = this.parentNode;
+    li.remove()
 }
 
 // Only allows Numbers and one decimal pont in textbox input
-function isNumberKey(evt){
+function isNumberKey(evt) {
     let charCode = (evt.which) ? evt.which : evt.keyCode
-    if (charCode == 46 && this.value.indexOf('.') != -1){
+    if (charCode == 46 && this.value.indexOf('.') != -1) {
         return false;
-    } else if (charCode > 31 && charCode != 46 && (charCode < 48 || charCode > 57 )){
+    } else if (charCode > 31 && charCode != 46 && (charCode < 48 || charCode > 57)) {
         return false;
     } return true;
 }
